f13606bf2bda6713cfe73129265f88b6531df513
IceRiver <luobupofly@gmail.com>
2026-03-26 23:47:06 +0800

    使用小游戏的方式

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   root: '.',
   build: {
     outDir: 'dist',
     target: 'esnext',
+    rollupOptions: {
+      input: {
+        main:     resolve(__dirname, 'index.html'),
+        tinyTown: resolve(__dirname, 'games/tiny-town/index.html'),
+      },
+    },
   },
 });

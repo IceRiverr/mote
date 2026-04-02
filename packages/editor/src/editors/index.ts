6f6a00293de20b26b9b188fef5fa8fb470ad2ced
IceRiver <luobupofly@gmail.com>
2026-04-03 00:16:43 +0800

    editor: 删除版2，继续第三个版本

--- packages/editor/src/editors/index.ts
+++
@@ -1,4 +0,0 @@
-export { ViewportEditor } from "./ViewportEditor.js";
-export { SceneTreeEditor } from "./SceneTreeEditor.js";
-export { InspectorEditor } from "./InspectorEditor.js";
-export { AssetBrowserEditor } from "./AssetBrowserEditor.js";
